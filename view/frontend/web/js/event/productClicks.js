1d49175c05e6c1f5f44e731d8a42b7a6aa2d88ab
Marcus Venghaus <info@inklammern.de>
2020-09-18 12:34:42 +0200

    product click event timeout

--- view/frontend/web/js/event/productClicks.js
+++ view/frontend/web/js/event/productClicks.js
@@ -39,7 +39,8 @@ define(['jquery'], function ($) {
                 },
                 'eventCallback': function () {
                     location.href = url;
-                }
+                },
+                'eventTimeout': 500
             };
 
             window.dataLayer.push(dataLayerData);
